0462ab0a49c6686d988977ab0f9fac3e68e47fb5
Jungho Park <122625228+pjh000104@users.noreply.github.com>
2024-10-02 12:26:32 -0400

    Combined writing files and the starting menu. Now a json file is writting with the input data

--- main_menu.py
+++ main_menu.py
@@ -1,14 +1,26 @@
+import json
+
+
 def start_software():
-    expenses = {'total': ' '                 
+    expenses = {'total': ' '
                 }
-    categories = ['cat1', 'cat2']
+    
+    create_file()
+    data = get_data()
+
+    categories = ['total_balance', 'food', 'house_hold', 'clothing', 
+                  'personal_expense', 'subscription',
+                  'housing_expense', 'insurance', 'other']
+    
     print("Hello welcome to the Personal_Expense")
 
     while True:
         print("Please enter the total expense: ")
         totalExpense = input()
+
         if is_float(totalExpense):
             expenses[totalExpense] = totalExpense
+            data["total_balance"] = totalExpense
             break
         else:
             print("Error entering value")
@@ -18,10 +30,13 @@ def start_software():
         while True:
             expense = input()
             if is_float(totalExpense):
+                data[c] = expense
                 expenses[c] = expense
                 break
             else:
                 print("Error entering value")
+    with open("info.json", "w") as file:
+        json.dump(data, file)
 
 
 def is_float(value):
@@ -34,4 +49,17 @@ def is_float(value):
         return False
 
 
+def create_file():
+    with open("info.json", "w") as file:
+        json.dump({
+                "total_balance":0, "food":0, "house_hold":0, "clothing":0,
+                "personal_expense":0, "subscription":0,"housing_expense":0,
+                "insurance":0,"other":0}, file)
+        
+
+# A function which return's the dictionary/hashmap within the file          
+def get_data():
+    with open('info.json', 'r') as file:
+        data = json.load(file)
+        return data
 start_software()
